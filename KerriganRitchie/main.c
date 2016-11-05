4858a0f1720f51afa73b18ec6fbf8d2abe7c5632
Deniz Kaya <denizkaya@denizs-macbook-pro.local>
2016-11-05 10:50:08 +0200

    Initial Commit

---
+++ KerriganRitchie/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  KerriganRitchie
+//
+//  Created by Deniz Kaya on 05/11/16.
+//  Copyright © 2016 Deniz Kaya. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
