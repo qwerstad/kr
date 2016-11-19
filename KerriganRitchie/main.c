a3a639b06f0c91677704e51853c5974f9dbdaa7e
Deniz Kaya <denizkaya@denizs-macbook-pro.local>
2016-11-19 18:30:06 +0200

    Exercise 1-13

--- KerriganRitchie/main.c
+++ KerriganRitchie/main.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
 
+// Define states for INSIDE or OUTSIDE a word
+#define INSIDE 1
+#define OUTSIDE 0
+
 int main(void){
-    int c, pc = EOF;
+    int c, state = INSIDE, currentlength = 0;
     
-    while ((c=getchar()) != EOF) {
-        if ((c != ' ')&&(c != '\t')) {
-            putchar(c);
-        }else if ((pc != ' ')&&(pc != '\t')) {
-            putchar('\n');
+    // The array wordcount keeps the number of occurences of a word of length wordcount [length]
+    int wordcount[30];
+    
+    // Initialize wordcount to 0 for all values of length
+    for (int i = 0; i < 30; ++i){
+        wordcount[i] = 0;
+    }
+    
+    // Start reading the input stream
+    while ((c = getchar()) != EOF){
+        if ((c != ' ') && (c != '\t') && (c != '\n')){
+            ++currentlength;
+        } else {
+            ++wordcount[currentlength];
+            currentlength = 0;
+            state = OUTSIDE;
         }
-        pc = c;
+    }
+    
+    // Print the histogram
+    for (int i = 1; i < 30; ++i){
+        printf("%2d: ", i);
+        for (int j = 0; j < wordcount[i]; ++j){
+            printf("%c", '|');
+        }
+        printf("\n");
     }
 }
