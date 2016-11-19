444413073db3583c42737bda51b5c497e5813e6e
Deniz Kaya <denizkaya@denizs-macbook-pro.local>
2016-11-19 18:26:25 +0200

    Exercise 1-6

--- KerriganRitchie/main.c
+++ KerriganRitchie/main.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 
-// print Fahrenheit-Celsius Table for
-// fahr = 0, 20, .., 300
-
-#define UPPER 100
-#define LOWER 0
-#define STEP 1
-
 int main(void)
 {
-    float fahr;
-    printf("%s\t%s\n", "Fahr", "Celsius");
+    char c;
     
-    for (fahr = UPPER; fahr >= LOWER; fahr = fahr-STEP){
-        printf("%4.0f\t%7.1f\n", fahr, (5.0 / 9.0 * (fahr-32)));
+    while ((c = getchar()) != EOF){
+        putchar(c);
     }
 }
