2de247a293c1a4da6c58bcab1fbe346087505b83
Deniz Kaya <denizkaya@denizs-macbook-pro.local>
2016-11-19 21:21:17 +0200

    Prior to Exercise 1-16

--- KerriganRitchie/main.c
+++ KerriganRitchie/main.c
@@ -1,19 +1,47 @@
 #include <stdio.h>
+#define MAXLINE 1000 /* maximum input line size*/
 
-#define UPPER 500
-#define LOWER 0
-#define STEP 20
+int getlineex(char line[], int maxline);
+void copy(char to[], char from[]);
 
-float fahrtocels(int fahr);
-
-int main(void){
-    printf("%s\t%s\n", "Fahr", "Celsius");
+/* print longest input line */
+main()
+{
+    int len;
+    int max;
+    char line[MAXLINE];
+    char longest[MAXLINE];
     
-    for (int fahr = LOWER; fahr <= UPPER; fahr = fahr+STEP){
-        printf("%4d\t%7.1f\n", fahr, fahrtocels(fahr));
-    }
+    max = 0;
+    while ((len = getlineex(line, MAXLINE)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+    if (max > 0)
+        printf("%s", longest);
+    return 0;
 }
 
-float fahrtocels(int fahr){
-    return (5.0 * (fahr-32) / 9.0);
+int getlineex(char s[], int lim)
+{
+    int c, i;
+    
+    for (i = 0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void copy(char to[], char from[])
+{
+    int i;
+    
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
 }
