602a30c571aca5a1dac6f026b6bd50bf316cf1d1
Deniz Kaya <denizkaya@denizs-macbook-pro.local>
2016-11-19 18:30:54 +0200

    Exercise 1-14

--- KerriganRitchie/main.c
+++ KerriganRitchie/main.c
@@ -1,35 +1,35 @@
+//
+//  main.c
+//  KerriganRitchie
+//
+//  Created by Deniz Kaya on 05/11/16.
+//  Copyright © 2016 Deniz Kaya. All rights reserved.
+//
+
 #include <stdio.h>
 
-// Define states for INSIDE or OUTSIDE a word
-#define INSIDE 1
-#define OUTSIDE 0
-
 int main(void){
-    int c, state = INSIDE, currentlength = 0;
+    int c;
     
-    // The array wordcount keeps the number of occurences of a word of length wordcount [length]
-    int wordcount[30];
+    // The array charcount keeps the number of occurences of a character
+    int charcount[224];
     
-    // Initialize wordcount to 0 for all values of length
-    for (int i = 0; i < 30; ++i){
-        wordcount[i] = 0;
+    // Initialize charcount to 0 for all characters
+    for (int i = 0; i < 224; ++i){
+        charcount[i] = 0;
     }
     
     // Start reading the input stream
     while ((c = getchar()) != EOF){
-        if ((c != ' ') && (c != '\t') && (c != '\n')){
-            ++currentlength;
-        } else {
-            ++wordcount[currentlength];
-            currentlength = 0;
-            state = OUTSIDE;
+        if (c >= ' '){
+            ++charcount[c-' '];
         }
     }
     
     // Print the histogram
-    for (int i = 1; i < 30; ++i){
-        printf("%2d: ", i);
-        for (int j = 0; j < wordcount[i]; ++j){
+    for (int i = 0; i < 224; ++i){
+        printf("%c: ", i+32);
+        for (int j = 0; j < charcount[i]; ++j){
             printf("%c", '|');
         }
         printf("\n");
